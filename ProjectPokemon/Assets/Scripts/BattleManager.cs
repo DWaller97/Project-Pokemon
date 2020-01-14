615b964283141f2f631bd47f70cb7f07dfcddbad
Dan <dan.waller@unity3d.com>
2020-01-14 16:58:34 +0000

    Fighting with asset bundles

--- ProjectPokemon/Assets/Scripts/BattleManager.cs
+++ ProjectPokemon/Assets/Scripts/BattleManager.cs
@@ -10,17 +10,30 @@ public class BattleManager : MonoBehaviour
     void Start()
     {
         managerRef = GameManager.GetGameManager();
-        StartCoroutine(LoadTrainerPokemon(GameManager.trainer, enemyPosition.position));
-        StartCoroutine(LoadTrainerPokemon(managerRef.playerTrainer, playerPosition.position));
+        //Go through all the Pokemon and load any.
+        //Check for duplicates.
+        if(managerRef.playerTrainer.pokemon[0] == GameManager.trainer.pokemon[0]){
+            StartCoroutine(LoadTrainerPokemon(GameManager.trainer, enemyPosition.position));
+        }else{
+            StartCoroutine(LoadTrainerPokemon(managerRef.playerTrainer, playerPosition.position));
+            StartCoroutine(LoadTrainerPokemon(GameManager.trainer, enemyPosition.position));
+        }
     }
 
     IEnumerator LoadTrainerPokemon(Trainer trainer, Vector3 position){
-        AssetBundleCreateRequest req = AssetBundle.LoadFromFileAsync($"Assets/Bundles/Mac/Pokemon.{trainer.pokemon[0].species.name}");
+        AssetBundleCreateRequest req = AssetBundle.LoadFromFileAsync($"Assets/Bundles/Mac/pokemon.charizard");
+        if(req.assetBundle == null)
+            yield break;
         while(!req.isDone){
             yield return null;
         }
         Debug.Log(req.assetBundle.name);
-        Instantiate(req.assetBundle, position, Quaternion.identity);
+        AssetBundleRequest bundle = req.assetBundle.LoadAssetAsync<GameObject>(trainer.pokemon[0].name);
+        while(!bundle.isDone){
+            yield return null;
+        }
+        GameObject obj = (GameObject)bundle.asset;
+        Instantiate(obj, position, Quaternion.identity);
     }
 
 
